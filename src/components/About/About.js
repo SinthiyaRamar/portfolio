085dd5b6d06ff04683c9b81249ceb77f2bb29785
SinthiyaRamar <sinthiyasinthiya249@gmail.com>
2023-03-22 10:50:04 +0530

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,39 +1,46 @@
-import React from 'react'
-import about from './About.module.css'
+import React from "react";
+import about from "./About.module.css";
 export default function About() {
   return (
-    <div id="About" className={about.aboutpage} >
+    <div id="About" className={about.aboutpage}>
       <div className={about.aboutdiv}>
-        <div className={about.aboutmetitlediv} >
+        <div className={about.aboutmetitlediv}>
           <h1>About Me</h1>
         </div>
-        <div className={about.aboutdetailsdiv} >
-          <div className={about.aboutimagediv} >
-            <div className={about.aboutimgdiv} >
+        <div className={about.aboutdetailsdiv}>
+          <div className={about.aboutimagediv}>
+            <div className={about.aboutimgdiv}>
               <figure className={about.swing}>
-              <img className={about.aboutmeimg} src="images/developer.jpg" alt="" />
- 
+                <img
+                  className={about.aboutmeimg}
+                  src="images/developer.jpg"
+                  alt=""
+                />
               </figure>
             </div>
           </div>
-            <div className={about.aboucontentdiv} >
-              <div className={about.aboutdetails}>
-                <ul>
-                  <li>I am Sinthiya Ramar.</li>
-                  <li> I have completed my 12th standard .</li>
-                  <li>My native is Keelapvoor.</li>
-                  <li>I was born on November 28th, 2005.</li>
-                  <li>I love coding because it challenges me to think creatively and solve problems.</li>
-                  </ul> 
-              </div>
+          <div className={about.aboucontentdiv}>
+            <div className={about.aboutdetails}>
+              <ul>
+                <li>I am Sinthiya Ramar.</li>
+                <li> I have completed my 12th standard .</li>
+                <li>My native is Keelapvoor.</li>
+                <li>I was born on November 28th, 2005.</li>
+                <li>
+                  I love coding because it challenges me to think creatively and
+                  solve problems.
+                </li>
+              </ul>
             </div>
-       
-          
+          </div>
         </div>
-        <div className={about.mailiddiv} >
-                <h2><span className={about.mailidtext} >Mail : </span>sinthiya.r@zohocorp.com</h2>
-            </div>
+        <div className={about.mailiddiv}>
+          <h2>
+            <span className={about.mailidtext}>Mail : </span>
+            sinthiya.r@zohocorp.com
+          </h2>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,44 +1,62 @@
-import React,{useRef} from 'react'
-import contact from './Contacts.module.css'
-import emailjs from 'emailjs-com'
-
+import React, { useRef } from "react";
+import contact from "./Contacts.module.css";
+import emailjs from "emailjs-com";
 
 export default function Contacts() {
-  const form  = useRef(null);
-  function sendEmail(e){
-     e.preventDefault();
-    emailjs.sendForm('service_gxa4mah','template_4cr0dal',form.current,'ZOTKcBU4zy14injyW').then(res=>{
-      console.log(res);
-    }).catch(err=>{
-       console.log(err);
-    });
+  const form = useRef(null);
+  function sendEmail(e) {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_gxa4mah",
+        "template_4cr0dal",
+        form.current,
+        "ZOTKcBU4zy14injyW"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     e.target.reset();
   }
   return (
-    <div id="Contact" className={contact.contactpage} >
+    <div id="Contact" className={contact.contactpage}>
       <div className={contact.contactwholediv}>
         <div className={contact.titlediv}>
           <h1>Contact</h1>
         </div>
         <div className={contact.contactdivs}>
           <div className={contact.gifdiv}>
-            <img className={contact.gifimg} src="./images/contact.gif" alt=""/>
+            <img className={contact.gifimg} src="./images/contact.gif" alt="" />
           </div>
-        <form  ref={form} onSubmit={sendEmail} className={contact.form} >
+          <form ref={form} onSubmit={sendEmail} className={contact.form}>
             <div className={contact.inputdiv}>
-             
-              <label name="labelusername" >User Name</label>
-              <input name="name" className={contact.username} type="text" autoComplete='off'/>
+              <label name="labelusername">User Name</label>
+              <input
+                name="name"
+                className={contact.username}
+                type="text"
+                autoComplete="off"
+              />
               <label name="labelusername">Email Address</label>
-              <input name="mail" className={contact.userid} type="text" autoComplete='off' />
-              <label name="usernamedescription" >Description</label>
-              <textarea  name="message" className={contact.description} ></textarea>
-              <input className={contact.submit} type="submit"/>
+              <input
+                name="mail"
+                className={contact.userid}
+                type="text"
+                autoComplete="off"
+              />
+              <label name="usernamedescription">Description</label>
+              <textarea
+                name="message"
+                className={contact.description}
+              ></textarea>
+              <input className={contact.submit} type="submit" />
             </div>
-            </form>
-
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
